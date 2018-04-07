eb508130e97d0d7662fb57881fdf35af21407015
ConorHogan <36172554+ConorHogan@users.noreply.github.com>
2018-04-07 17:09:52 +0100

    Rewatched lectures and refactored

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,8 +1,8 @@
-# Found this one difficult for some reason. Couldn't get the data to print correctly.
+#Please complete the following exercise this week.
+#Write a Python script that reads the Iris data set in and prints the four numerical values on each row in a nice format.
+#That is, on the screen should be printed the petal length, petal width, sepal length and sepal width, and these values should have the decimal places aligned, with a space between the columns.
 
 with open("iris.csv") as iris_data:
 
   for line in iris_data:
-    line_as_string = line.split(",")
-    string = str(line_as_string)
-    print(string[0])
+    print('{:1} {:1} {:1} {:1}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
