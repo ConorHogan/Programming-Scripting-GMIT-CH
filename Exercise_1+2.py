0c05c085618ec09b78d4b605c519792256339eeb
ConorHogan <36172554+ConorHogan@users.noreply.github.com>
2018-04-08 14:38:31 +0100

    Added comments

--- Exercise_1+2.py
+++ Exercise_1+2.py
@@ -1,4 +1,13 @@
 #Exercise 1 + 2 
+#Comment 1: My name is Conor, so the first and last letter of my name (C + R= 3 + 18) give the number  21. The 21st Fibonacci number is 10,946. 
+
+#Comment 2:
+
+#My surname is Conor
+#The first letter H is number 72
+#The last letter n is number 110
+#Fibonacci number 182 is 48558529144435440119720805669229197641
+#Ord() is a built in function in Python that returns the ASCII code for a charecter.
 
 def fib(n):
   """This function returns the nth Fibonacci number."""
